eca089c74d6ad4ddc5c8433c5387d0f3e0e055ae
Paul Brian Marcuelo <marcuelo1@gmail.com>
2023-02-26 01:10:49 +0800

    added sign in page

---
+++ src/App.jsx
@@ -0,0 +1,69 @@
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Loading from './app/core/utils/loading';
+
+import Layout2 from './/app/core/components/layouts/layout2';
+
+import DashboardIcon from "./app/core/assets/icons/dashboard.png";
+import DashboardIconWhite from "./app/core/assets/icons/dashboard-white.png";
+import DeliveryIcon from "./app/core/assets/icons/delivery.png";
+import DeliveryIconWhite from "./app/core/assets/icons/delivery-white.png";
+import StationReportsIcon from "./app/core/assets/icons/stationreports.png";
+import StationReportsIconWhite from "./app/core/assets/icons/stationreports-white.png";
+import SuppliersIcon from "./app/core/assets/icons/suppliers.png";
+import SuppliersIconWhite from "./app/core/assets/icons/suppliers-white.png";
+import BranchesIcon from "./app/core/assets/icons/branches.png";
+import BranchesIconWhite from "./app/core/assets/icons/branches-white.png";
+import Logo from "./app/core/assets/doubledragon.jpg";
+import PortalIndexView from './app/features/portal/views/portalIndexView';
+
+const tabs = [
+  {
+    urlName: "",
+    tabName: "Dashboard",
+    activeTabIcon: DashboardIcon,
+    inactiveTabIcon: DashboardIconWhite
+  },
+  {
+    urlName: "inventory",
+    tabName: "Inventory",
+    activeTabIcon: DeliveryIcon,
+    inactiveTabIcon: DeliveryIconWhite
+  },
+  {
+    urlName: "sales",
+    tabName: "Sales",
+    activeTabIcon: StationReportsIcon,
+    inactiveTabIcon: StationReportsIconWhite
+  },
+  {
+    urlName: "customers",
+    tabName: "Customers",
+    activeTabIcon: SuppliersIcon,
+    inactiveTabIcon: SuppliersIconWhite
+  },
+  {
+    urlName: "logs",
+    tabName: "Logs",
+    activeTabIcon: BranchesIcon,
+    inactiveTabIcon: BranchesIconWhite
+  }
+]
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Loading />
+        <Routes>
+          <Route path="sign_in" element={<PortalIndexView />} />
+          <Route path="/" element={<Layout2 tabs={tabs} logo={Logo} name="Library System" />} >
+          </Route>
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/app/features/portal/views/portalIndexView.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import ActionButton2 from '../../../core/components/buttons/actionButton2';
+import "../stylesheets/index.scss";
+
+class PortalIndexView extends Component {
+  state = {  } 
+  render() { 
+    const signInBtnConfig = {
+      name: "Sign In", 
+      className: "signin-btn",
+      onClick: () => {},
+      theme: "primary"
+    };
+
+    return (
+      <div id="portal-index-view">
+        <div className="content-card">
+          <div className="section">
+            <div className="title">Welcome Back!</div>
+            <div className="group">
+              <div className="label">Email</div>
+              <input type="email" name="email" id="" placeholder='Enter Email' />
+            </div>
+            <div className="group">
+              <div className="label">Password</div>
+              <input type="password" name="password" id="" placeholder='Enter Password' />
+            </div>
+            <ActionButton2 config={signInBtnConfig} />
+            <div className="signup-btn-content">
+              Don't have an account yet?
+              <span> Sign Up </span>
+              now!
+            </div>
+          </div>
+          <div className="section">
+            <img src={require("../../../core/assets/samples/book.png")} alt="" />
+            <div className="subtitle">Book - Harry Potter</div>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam beatae necessitatibus quasi corporis harum consequuntur. Esse velit aliquid tempora, eligendi veniam earum explicabo unde officiis repellat quas, nisi deserunt voluptatibus.</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+ 
+export default PortalIndexView;
